d40a6fd94568fe6c143d698a5a1eeb856fba94c9
Antero <tiusanen.antti@gmail.com>
2024-04-02 16:28:14 +0300

    added frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 morgan.token('req-body', (req) => {
@@ -10,6 +11,7 @@ morgan.token('req-body', (req) => {
   return ''
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time :req-body'))
+app.use(cors())
 
 let persons = [
   {
@@ -103,7 +105,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
