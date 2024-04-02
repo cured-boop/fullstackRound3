301ea8dffc2d15d6609cb362fa97aa4ea4bb7abd
Antero <tiusanen.antti@gmail.com>
2024-04-02 14:18:31 +0300

    3.8

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+app.use(express.json())
+morgan.token('req-body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time :req-body'))
 
 let persons = [
   {
@@ -39,6 +49,58 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const randId = Math.floor(Math.random() * 10000) + 1
+  return randId
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'missing name' 
+    })
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: 'missing number'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+  const uniqueName = persons.find(person => person.name === body.name)
+  if (uniqueName) {
+    return response.status(400).json({
+      error: 
+      'name must be unique'
+    })
+  }
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
